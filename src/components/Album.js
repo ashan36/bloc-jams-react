b1ba3c02f136ebca5cdecb7862c36c3041831528
ashan36@gmail.com <ashan36@gmail.com>
2018-10-11 12:32:06 -0400

    Routing assignment complete

---
+++ src/components/Album.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Album extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section className="album">
+        Album page will go here
+      </section>
+    );
+  }
+}
+
+export default Album;

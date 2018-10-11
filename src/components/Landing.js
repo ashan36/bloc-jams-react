f1d4b2ffd03ee6ff2418bac873c50451366e4c5c
ashan36@gmail.com <ashan36@gmail.com>
2018-10-11 11:44:34 -0400

    Routing checkpoint complete

---
+++ src/components/Landing.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section className="landing">
+        Landing page will go here
+      </section>
+    );
+  }
+}
+
+export default Landing;
---
+++ src/components/Library.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Library extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section className="library">
+        Library will go here
+      </section>
+    );
+  }
+}
+
+export default Library;
